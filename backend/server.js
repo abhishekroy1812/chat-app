136a5aee0daf2747abbf1e5fff9481fd22fd2b6f
Abhishek Roy <royabhishek0225@gmail.com>
2022-08-19 14:49:34 +0530

    ready fo deployment

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ dotenv.config();
 connectDB();
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 // app.get("/", (req, res) => {
 //     res.send("api is running")
